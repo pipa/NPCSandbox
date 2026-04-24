3756021a8d9ef141136da1072d8414f5ea16db26
Pipa <luis.matute@me.com>
2026-04-24 22:38:37 +0200

    Initial Commit

---
+++ NPCSandbox macOS/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  NPCSandbox macOS
+//
+//  Created by Luis Matute on 24/04/2026.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
